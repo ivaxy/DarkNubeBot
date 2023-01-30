fa2cfc99c5224a54eb472d54c561ba643232d842
ivaxy <103711414+ivaxy@users.noreply.github.com>
2023-01-30 00:10:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['tonyrlyrical'] #
-godlist = ['tonyrlyrical'] #
+tl_admin_users = "tonyrlyrical" #
+godlist = "tonyrlyrical" #
 
 async def get_root(username):
     if os.path.isdir(config.ROOT_PATH+username)==False:
