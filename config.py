ae74fe9935a0aaf858a58994a76ae6bf773cbfc4
ivaxy <103711414+ivaxy@users.noreply.github.com>
2023-01-30 00:14:08 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import ProxyCloud
 BOT_TOKEN =  os.environ.get('bot_token','5603762128:AAEUkKQ3lVh7WDdRxj8ebNjHmKb01OofNKk')
 API_ID =  os.environ.get('api_id','15158057')
 API_HASH = os.environ.get('api_hash','6a7956a4a75bd2ed46a021649e4c9edf')
-SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
+SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','390'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tonyrlyrical').split(';')
+ACCES_USERS = os.environ.get('tonyrlyrical')
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
 
 if PROXY:
